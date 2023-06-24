4bbf1ade32d556f4c6e40107c0c19088a904fa0a
rajalakshmibharath <rajalakshmibharath22@gmail.com>
2023-06-24 18:56:28 +0200

    logging,exception & utils

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "cnnClassifier"  #giving project name is optional (chk this)
+project_name = "Crop_rec and Dis_pred"  #giving project name is optional (chk this)
 
 list_of_files = [
     ".github/workflows/.gitkeep",
